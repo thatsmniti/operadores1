a293a352ec0f5ac505a3ea8bb072e28210110ab6
thatsmniti <134503971+thatsmniti@users.noreply.github.com>
2023-09-20 07:51:52 -0300

    Add files via upload

---
+++ operadores1/operadores.js
@@ -0,0 +1,23 @@
+function soma(){
+var s1 = document.getElementById("valor1").value;
+var s2 = document.getElementById("valor2").value;
+var s3 = parseInt(s1) + parseInt(s2);
+
+alert (s3);
+}
+
+function multi(){
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) * parseInt(s2);
+    
+    alert (s3);
+}
+
+function divis(){
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) / parseInt(s2);
+    
+    alert (s3);
+}
